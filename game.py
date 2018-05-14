d24ed0d1ec89c132e18fa395ff2024fa5557051d
Marcin Gren <marcin.gren@gmail.com>
2018-05-14 21:47:05 +0200

    checking and guess printing

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 
-def answerWriter():
+def hiddenAnswerWriter():
     for letters in range(lengthOfTheAnswer):
         listOfPhraseBeingGuessed.append("_ ")
     return listOfPhraseBeingGuessed
@@ -8,22 +8,41 @@ def createPhrase(listOfPhrase):
     phraseBeingGuessed = ''.join(listOfPhrase)
     return phraseBeingGuessed
 
+def checkIfTheLetterIsInTheAnswer(letter):
+    for i in range(lengthOfTheAnswer):
+        if letter == answerBeingGuessed[i]:
+            listOfPhraseBeingGuessed[i] = answerBeingGuessed[i]
+    phraseAnswer = createPhrase(listOfPhraseBeingGuessed)
+    return phraseAnswer
+
+def guesser(phraseAnswer):
+    print("Guess the phrase: " + phraseAnswer)
+    phraseAnswer = checkIfTheLetterIsInTheAnswer(letterUsed)
+    return phraseAnswer
+
 listOfPhraseBeingGuessed = []
 answer = "test"
+answerBeingGuessed = list(answer)
 lengthOfTheAnswer = len(answer)
+listAnswer = hiddenAnswerWriter()
+phraseAnswer = createPhrase(listAnswer)
+
+print(answerBeingGuessed)
 
 
 
-letterUsed = "a" #input("Please enter the letter: ")
+letterUsed = "e" #input("Please enter the letter: ")
 if len(letterUsed) > 1:
     print("Please use one letter!")
 
-listAnswer = answerWriter()
-phraseAnswer = createPhrase(listAnswer)
 
-print("Guess the phrase: " + phraseAnswer)
+phraseAnswer = guesser(phraseAnswer)
+
+phraseAnswer = guesser(phraseAnswer)
 
 
+
+print("**************")
 print(letterUsed)
 print("asd")
 
