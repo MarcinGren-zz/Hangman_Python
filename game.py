f324258e5ee25a1670a0787fcd5e30c4885465c9
Marcin Gren <marcin.gren@gmail.com>
2018-05-17 20:37:59 +0200

    used letters mechanism

--- game.py
+++ game.py
@@ -20,13 +20,21 @@ def guesser(phraseAnswer):
     print("Guess the phrase: " + phraseAnswer)
     return phraseAnswer
 
-def typeLetter():
+def typeLetter(usedLetters):
+    print("Letters used: " + str(usedLetters))
     letterUsed = input("Please enter the letter: ")
     if len(letterUsed) > 1:
         print("Please use one letter!")
         letterUsed = ""
+    usedLetters = lettersUsedList(letterUsed)
     return letterUsed
 
+def lettersUsedList(letter):
+    if letter in usedLetters:
+        print("Letter already tried, please pick another.")
+    else:
+        usedLetters.append(letter)
+        return usedLetters
 
 
 listOfPhraseBeingGuessed = []
@@ -36,16 +44,19 @@ lengthOfTheAnswer = len(answer)
 listAnswer = hiddenAnswerWriter()
 phraseAnswer = createPhrase(listAnswer)
 letterUsed = ""
+usedLetters = []
 
-print(answerBeingGuessed)
+#print(answerBeingGuessed)
 
 for i in range(10):
     phraseAnswer = guesser(phraseAnswer)
     if listOfPhraseBeingGuessed == answerBeingGuessed:
         print("Congratulations! You've guessed it!")
         break
-    letterUsed = typeLetter()
+    letterUsed = typeLetter(usedLetters)
 
+# if listOfPhraseBeingGuessed == answerBeingGuessed:
+#     print("Congratulations! You've guessed it!")
 
 # phraseAnswer = guesser(phraseAnswer)
 # letterUsed = typeLetter()
